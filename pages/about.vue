e976f7c146eed50fe91a1ce95d525a96ae13ffc9
yuchao.feng <yuchao.feng@yiducloud.cn>
2020-12-02 17:20:26 +0800

    初始化nuxt项目

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>关于</h1>
+</template>
+
+<script>
+    export default {
+        name: "about"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>hello nuxtjs</h1>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
